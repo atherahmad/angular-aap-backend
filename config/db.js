bb937b0eeba9521bcf26cc7b4038870b014e0958
Ather Ahmad <atherahmad49@yahoo.com>
2022-07-18 17:54:43 +0200

    sendgrid added and nodemailer removed

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const connectDB = async () => {
 
     const mongoose = require("mongoose");
     require('dotenv').config();
-    const db =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zij5h.mongodb.net/${process.env.DB_HOST}`
+    const db = process.env.DB_URI
 
 
 // Ather  const db = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-er0mu.gcp.mongodb.net/${process.env.DB_HOST}?retryWrites=true&w=majority`;
@@ -10,14 +10,13 @@ const connectDB = async () => {
     try {
         await mongoose.connect(db, {
             useNewUrlParser: true,
-            useCreateIndex: true,
             useUnifiedTopology: true,
-            useFindAndModify: false
+
         });
         console.log('Mongo Atlas server is ready');
     } catch (error) {
 
-        process.exit(1);
+        console.log(error.message);
     }
 
 }
